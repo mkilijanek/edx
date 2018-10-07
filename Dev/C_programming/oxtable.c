e81e4bcae2f7ef7a68032bea2298bb820923aa13
mkili-Lenovo\mkili <kilijanek@gmail.com>
2018-10-07 16:33:01 +0200

    corrected bracket issue within source file

--- Dev/C_programming/oxtable.c
+++ Dev/C_programming/oxtable.c
@@ -10,11 +10,12 @@ int main(void) {
     printf("+\n");
 
     for (i = 0; i < 3; i++)
+	{
         printf("| o | X | o | X | o | X |");
         printf("\n");
         printf("| X | o | X | o | X | o |");
         printf("\n");
-
+	}
     printf("+");
     for (i = 0; i < 23; i++)
         printf("-");
