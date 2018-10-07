9cb413be108f4cb030465119ee492356399a14d8
mkili-Lenovo\mkili <kilijanek@gmail.com>
2018-10-07 15:12:06 +0200

    updated test.py with new content

---
+++ test.py
@@ -0,0 +1,25 @@
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[0])
+
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[2])
+
+my_list = [5, 'old', 'new', 8, 'time', 2]
+print(my_list[4])
+
+my_list = [2.3, 'dog', 34, 'cat', 52]
+print(my_list[-1])
+
+my_list = [2.3, 'dog', 34, 'cat', 52]
+print(my_list[-5])
+
+x = [5, 'dog']
+y = ['cat', 3.5]
+print(x + y)
+
+x = [ 5, 'dog']
+print(3 * x)
+
+x = [5, 'dog']
+y = ['cat']
+print(x + 2 * y)
